a05838e71efa95c6aae6e508adce3e392c115d75
Igor Massaro <igorzagomassaro@gmail.com>
2024-08-14 19:43:47 -0300

    add status bar

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
+import { StatusBar } from 'react-native';
 import Router from './src/Router';
 import { CountryProvider } from './src/data/contexts/CountriesContext'
 
 export default function App() {
   return (
     <CountryProvider>
+      <StatusBar backgroundColor="#000" />
       <Router />
     </CountryProvider>
   );
